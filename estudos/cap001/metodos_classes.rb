ce786de99fab637b7a2d2f6a0bf6a263306ba8bc
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-26 00:01:03 -0300

    Estudos capitulo 1

---
+++ estudos/cap001/metodos_classes.rb
@@ -0,0 +1,14 @@
+# Para descobrir quais metodos estão disponíveis para determinado objeto de uma determinada classe basta digitar .methods
+puts 42.methods
+
+puts "Teste".methods
+
+# Para descobrir qual classe determinado objeto pertence basta digitar .class
+puts 42.class
+
+puts "Teste".class
+
+puts true.class # Booleano é chamado de TrueClass em Ruby
+
+
+
