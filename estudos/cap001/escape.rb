8b68ac05c1840322d66183689a66f85314079da2
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-26 00:00:40 -0300

    Estudos capitulo 1

---
+++ estudos/cap001/escape.rb
@@ -0,0 +1,13 @@
+# Sequências de escape em strings
+
+puts "Teste nova linha \n Nova linha "
+
+puts "Teste de tabulação \n\t Nova tabulação"
+
+puts "Teste de aspas duplas para uma \"citação\""
+
+puts "Teste de aspas simples para outro \'exemplo\'"
+
+puts "Teste de barra invertida para especificar um \\caminho\\exemplo"
+
+puts "Estes exemplos são funcionais nas strings entre aspas \" entre aspas \' as escapes são tratadas de forma literal"
