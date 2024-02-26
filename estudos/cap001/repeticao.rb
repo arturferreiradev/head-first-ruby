994318c26405438b7e9a80352bdedd380d6118a3
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-02-26 00:01:10 -0300

    Estudos capitulo 1

---
+++ estudos/cap001/repeticao.rb
@@ -0,0 +1,13 @@
+# While - estrutura de repetição enquanto
+number = 1
+while number <= 5
+    puts number
+    number += 1
+end
+
+# Until - estrutura de repetição até que
+until number > 10
+    puts number
+    number += 1
+end
+
